685d02d302ef5e9a9552b57f64010d6c3ad13bfe
jeremingo <58335558+jeremingo@users.noreply.github.com>
2024-11-13 23:14:41 +0200

    gh-5 Connect to MongoDB

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 const indexRouter = require('./routes');
 const postsRouter = require('./routes/post');
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (err) => { console.error(err) });
+db.on('open', () => { console.log('connected to mongo') });
+
 const app = express();
 const port = process.env.PORT;
 
