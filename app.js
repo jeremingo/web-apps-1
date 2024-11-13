5949a38ca594610ce49140de204fc87fe784a380
jeremingo <58335558+jeremingo@users.noreply.github.com>
2024-11-13 15:37:08 +0200

    gh-3 Create Posts Router and Controller, and Index Router

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 
+const indexRouter = require('./routes');
+const postsRouter = require('./routes/post');
+
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('app');
-});
+app.use('/', indexRouter);
+app.use('/post', postsRouter);
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
