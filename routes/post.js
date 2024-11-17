5fb4b12b68d3ff4e5b7250d3777debd0725cfc25
Ripshtos <ohadrip@gmail.com>
2024-11-17 22:08:48 +0200

    added request.rest to comments , added post update feature

--- routes/post.js
+++ routes/post.js
@@ -6,5 +6,7 @@ const Post = require('../controllers/post');
 router.get('/', Post.getPosts);
 router.get('/:id', Post.getPostById)
 router.post('/', Post.addPost);
+router.put('/:id', Post.updatePost);
+
 
 module.exports = router;
